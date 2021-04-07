4edbe370c5bf51415b3956eac204d4628257d612
Aravind <aravind08222@gmail.com>
2021-04-07 15:49:11 +0530

    changes

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -112,7 +112,6 @@ const Score = ({gameScore, scores, saveTop5Scores, loading, firbaseId}) => {
                 <h4 style={{marginTop: 8, marginBottom: 15}}>Your Score is: {gameScore}</h4>
                 <h5 style={{marginTop: 55, marginBottom: 15, fontSize: 20}}>Best 5 Score of All Time</h5>
             </div>
-            <button onClick={dialogOpenHandler}>Open</button>
             {dialogBox}
             <table className={classes.customers}>
                 <thead>
